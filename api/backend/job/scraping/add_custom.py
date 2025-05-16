72fba5f4ee477242fdd0423996d966d0c4a84562
Jayden Pyles <jpylesbusiness@gmail.com>
2025-05-16 13:52:18 -0500

    feat: add tests for adding custom cookies/headers

--- api/backend/job/scraping/add_custom.py
+++ api/backend/job/scraping/add_custom.py
@@ -1,7 +1,7 @@
 from typing import Any, Optional
 from urllib.parse import urlparse
 
-from playwright.async_api import Page
+from playwright.async_api import Page, BrowserContext
 
 import logging
 
@@ -11,32 +11,21 @@ LOG = logging.getLogger(__name__)
 async def add_custom_cookies(
     custom_cookies: list[dict[str, Any]],
     url: str,
-    page: Page,
+    context: BrowserContext,
 ) -> None:
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+
     for cookie in custom_cookies:
-        domain = cookie.get("domain", "")
+        cookie_dict = {
+            "name": cookie.get("name", "default_name"),
+            "value": cookie.get("value", "default_value"),
+            "domain": domain,
+            "path": "/",
+        }
 
-        if "domain" not in cookie:
-            parsed_url = urlparse(url)
-            domain = parsed_url.netloc
-
-        cookie["domain"] = domain
-        cookie["path"] = "/"
-        cookie["name"] = cookie.get("name", "")
-        cookie["value"] = cookie.get("value", "")
-        cookie["expires"] = cookie.get("expires", 0)
-        cookie["httpOnly"] = cookie.get("httpOnly", False)
-        cookie["secure"] = cookie.get("secure", False)
-        same_site = cookie.get("sameSite", "Lax")
-
-        if same_site is None:
-            same_site = "Lax"
-
-        cookie["sameSite"] = (
-            same_site.capitalize() if same_site.lower() in ["lax", "strict"] else "Lax"
-        )
-        LOG.info(f"Adding cookie: {cookie}")
-        await page.context.add_cookies([cookie])  # type: ignore
+        LOG.info(f"Adding cookie: {cookie_dict}")
+        await context.add_cookies([cookie_dict])  # type: ignore
 
 
 async def add_custom_headers(
@@ -53,7 +42,7 @@ async def add_custom_items(
     headers: Optional[dict[str, Any]] = None,
 ) -> None:
     if cookies:
-        await add_custom_cookies(cookies, url, page)
+        await add_custom_cookies(cookies, url, page.context)
 
     if headers:
         await add_custom_headers(headers, page)
