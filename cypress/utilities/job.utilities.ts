f0a0822a6c88e0fb502be5305178844612dbdcbd
Jayden Pyles <jpylesbusiness@gmail.com>
2025-06-01 14:06:32 -0500

    fix: cypress tests

--- cypress/utilities/job.utilities.ts
+++ cypress/utilities/job.utilities.ts
@@ -78,8 +78,8 @@ export const waitForJobCompletion = (url: string) => {
   cy.contains("div", url, { timeout: 10000 }).should("exist");
   try {
     cy.get("[data-testid='job-status']", { timeout: 120000 }).should(
-      "have.text",
-      "Completed"
+      "include.text",
+      "completed"
     );
   } catch (error) {
     cy.log(`Job status was: ${error}`);
